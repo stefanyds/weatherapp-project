706599029c9986c9e58d14f9ddbdb8c407e678dc
stefanyds <ste.stefany22@gmail.com>
2023-10-02 14:58:19 -0300

    refactor: destructuring styles from CurrentWeather.js

--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -4,26 +4,37 @@ import { Feather } from '@expo/vector-icons'
 import RowText from "../components/RowText"
 
 const CurrentWeather = () => {
+  const { 
+    wrapper, 
+    container, 
+    temp, 
+    feels, 
+    highLowWrapper, 
+    highLow, 
+    bodyWrapper, 
+    description, 
+    message 
+  } = styles
   return (
-    <SafeAreaView style={styles.wrapper}>
-      <View style={styles.container}>
+    <SafeAreaView style={wrapper}>
+      <View style={container}>
         <Feather name="sun" size={100} color="black" />
-        <Text style={styles.temp}>6</Text>
-        <Text style={styles.feels}>Feels like 5</Text>
+        <Text style={temp}>6</Text>
+        <Text style={feels}>Feels like 5</Text>
         <RowText 
           messageOne={'High: 8'}
           messageTwo={'Low: 6'}
-          containerStyles={styles.highLowWrapper}
-          messageOneStyles={styles.highLow}
-          messageTwoStyles={styles.highLow}
+          containerStyles={highLowWrapper}
+          messageOneStyles={highLow}
+          messageTwoStyles={highLow}
         />
       </View>
         <RowText
           messageOne={'Its sunny'}
           messageTwo={'Its perfect t-shirt weather'}
-          containerStyles={styles.bodyWrapper}
-          messageOneStyles={styles.description}
-          messageTwoStyles={styles.message}
+          containerStyles={bodyWrapper}
+          messageOneStyles={description}
+          messageTwoStyles={message}
         />
           
     </SafeAreaView>
