e3ee3dfe436c8f561e3ba878b82bc1efc06065fe
stefanyds <ste.stefany22@gmail.com>
2023-10-04 18:36:14 -0300

    styling the Tabs component

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -12,7 +12,18 @@ const Tabs = () => {
         <Tab.Navigator 
         screenOptions={{
             tabBarActiveTintColor: 'tomato',
-            tabBarInactiveTintColor: 'grey'
+            tabBarInactiveTintColor: 'grey',
+            tabBarStyle: {
+                backgroundColor: 'lightblue'
+            },
+            headerStyle: {
+                backgroundColor: 'lightblue'
+            },
+            headerTitleStyle: {
+                fontWeight: 'bold',
+                fontSize: 25,
+                color: 'tomato'
+            }
         }}
       >
         <Tab.Screen 
