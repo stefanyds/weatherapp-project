a1fa8beac8547430977dd90fd782edd4494be99b
stefanyds <ste.stefany22@gmail.com>
2023-10-04 18:16:27 -0300

    feat: styling the tabs

--- App.tsx
+++ App.tsx
@@ -4,16 +4,58 @@ import City from "./src/screens/City"
 import CurrentWeather from "./src/screens/CurrentWeather"
 import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { Feather } from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator() //initialize the tab object
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name={'Current'} component={CurrentWeather}/>
-        <Tab.Screen name={'Upcoming'} component={UpcomingWeather} />
-        <Tab.Screen name={'City'} component={City} />
+      <Tab.Navigator 
+      screenOptions={{
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'grey'
+      }}
+      >
+        <Tab.Screen 
+          name={'Current'} 
+          component={CurrentWeather} 
+          options={{
+            tabBarIcon: ({ focused }) => (
+            <Feather 
+              name={'droplet'} 
+              size={25} 
+              color={focused ? 'tomato' : 'black'} 
+              />
+            )
+          }}
+        />
+        <Tab.Screen 
+          name={'Upcoming'} 
+          component={UpcomingWeather} 
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather 
+                name={'clock'} 
+                size={25} 
+                color={focused ? 'tomato' : 'black'} 
+              />
+            )
+          }} 
+        />
+        <Tab.Screen 
+          name={'City'} 
+          component={City}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather 
+                name={'home'} 
+                size={25} 
+                color={focused ? 'tomato' : 'black'} 
+              />
+            )
+          }} 
+        />
       </Tab.Navigator>
     </NavigationContainer>
   )
