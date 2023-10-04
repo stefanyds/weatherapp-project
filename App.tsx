1493e54deef4e3da870522c7b08e9be5204dd324
stefanyds <ste.stefany22@gmail.com>
2023-10-04 10:04:43 -0300

    feat: implementing the navigation tabs

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
 import React from "react"
-import { View, StyleSheet } from "react-native"
 import UpcomingWeather from "./src/screens/UpcomingWeather"
 import City from "./src/screens/City"
 import CurrentWeather from "./src/screens/CurrentWeather"
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+
+const Tab = createBottomTabNavigator() //initialize the tab object
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <CurrentWeather />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name={'Current'} component={CurrentWeather}/>
+        <Tab.Screen name={'Upcoming'} component={UpcomingWeather} />
+        <Tab.Screen name={'City'} component={City} />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-})
 export default App
